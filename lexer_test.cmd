9db0ce43b7f7ae4b102d66c390f1916bff3f7dee
Thanh, To <tmthanh@apcs.vn>
2017-12-03 20:06:53 +0700

    sf

---
+++ lexer_test.cmd
@@ -0,0 +1,3 @@
+cd C:\Users\admin\Downloads\Documents\APCS\Year3\Term1\Compiler design\HW\Lexer\MySolution\MyLexer\src
+javac -cp com/java-cup-11b-runtime.jar com/java_cup/runtime/*.java com/Util/*.java com/mylexer/MyLexer.java
+java -cp com/java-cup-11b-runtime.jar; com.mylexer.MyLexer com/test/%1
